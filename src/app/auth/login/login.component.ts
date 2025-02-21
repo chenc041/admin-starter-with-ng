313792fa0c8e95100baad0e91dd62d403f1cbe6c
chenc <double_cl@163.com>
2025-02-21 19:25:56 +0800

    fix(auth): 优化登录表单验证逻辑
    - 为复选框添加 p-invalid 类样式
    - 实现表单验证不通过时的错误提示
    - 更新 Angular相关依赖版本

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -5,11 +5,12 @@ import { InputTextModule } from 'primeng/inputtext';
 import { ButtonModule } from 'primeng/button';
 import { CheckboxModule } from 'primeng/checkbox';
 import { LoginService } from '~/app/auth/login/login.service';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [ReactiveFormsModule, FormsModule, PasswordModule, InputTextModule, ButtonModule, CheckboxModule],
+  imports: [ReactiveFormsModule, FormsModule, PasswordModule, InputTextModule, ButtonModule, CheckboxModule, NgClass],
   templateUrl: './login.component.html',
 })
 export default class LoginComponent {
@@ -22,8 +23,16 @@ export default class LoginComponent {
 
   handleLogin(): void {
     const { valid, value } = this.loginForm;
+    console.log(valid, 'valid');
     if (valid) {
       this.loginService.login(value).subscribe((res) => console.log(res));
+    } else {
+      Object.values(this.loginForm.controls).forEach((control) => {
+        if (control.invalid) {
+          control.markAsDirty();
+          control.updateValueAndValidity({ onlySelf: true });
+        }
+      });
     }
   }
 }
