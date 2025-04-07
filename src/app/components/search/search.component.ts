137d31f2152253ea206351f1c9d889bf8616344f
chenc <double_cl@163.com>
2025-04-07 21:29:10 +0800

    feat(search): 添加搜索组件并优化仪表板页面
    - 在仪表板页面添加搜索组件
    - 创建可配置的搜索组件，支持动态表单生成
    - 优化搜索组件的样式和交互，包括按钮和输入框的实现
    - 在 angular.json 中设置组件的变更检测策略为 OnPush

--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -1,31 +1,80 @@
-import { Component, inject, input, OnInit, output } from '@angular/core';
-import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { ChangeDetectionStrategy, Component, inject, input, OnInit, output } from '@angular/core';
+import { AbstractControl, FormBuilder, FormGroup, ReactiveFormsModule, ValidationErrors } from '@angular/forms';
+import { InputText } from 'primeng/inputtext';
+import { ButtonDirective, ButtonLabel } from 'primeng/button';
+import { Ripple } from 'primeng/ripple';
+import { TranslatePipe } from '~/app/pipes/translate.pipe';
+
+interface FormConfig {
+  type: string;
+  label: string;
+  placeholder: string;
+  prop: string;
+  formItem: (string | ((control: AbstractControl) => ValidationErrors | null))[];
+}
 
 @Component({
   selector: 'app-search',
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, InputText, ButtonDirective, ButtonLabel, Ripple, TranslatePipe],
   templateUrl: './search.component.html',
   styleUrl: './search.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class SearchComponent implements OnInit {
+  /**
+   * 响应表单
+   */
   searchForm!: FormGroup;
-  formBuilder = inject(FormBuilder);
-  searchItems = input([]);
-  onSearch = output();
 
+  /**
+   * 表单构建
+   */
+  formBuilder = inject(FormBuilder);
+
+  /**
+   * 外部传入的表单配置
+   */
+  formConfig = input<FormConfig[]>([]);
+
+  /**
+   * 响应表单提交事件
+   */
+  onSubmit = output();
+
+  /**
+   * 响应表单重置事件
+   */
+  onReset = output();
+
+  /**
+   * 组件初始化
+   */
   ngOnInit() {
-    this.searchForm = this.formBuilder.group({
-      search: ['', Validators.required],
-    });
+    const formGroup = this.formConfig().reduce(
+      (acc, cur) => {
+        acc[cur.prop] = cur.formItem;
+        return acc;
+      },
+      {} as Record<string, (string | ((control: AbstractControl) => ValidationErrors | null))[]>,
+    );
+    this.searchForm = this.formBuilder.group(formGroup);
   }
 
+  /**
+   * 响应表单提交事件
+   */
   handleSearch() {
+    console.log(this.searchForm.value);
     if (this.searchForm.valid) {
-      this.onSearch.emit(this.searchForm.value.search);
+      this.onSubmit.emit(this.searchForm.value);
     }
   }
 
+  /**
+   * 响应表单重置事件
+   */
   handleReset() {
     this.searchForm.reset();
+    this.onReset.emit();
   }
 }
--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { DatePickerModule } from 'primeng/datepicker';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, Validators } from '@angular/forms';
+import { SearchComponent } from '~/app/components/search/search.component';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [DatePickerModule, FormsModule],
+  imports: [DatePickerModule, FormsModule, SearchComponent],
   templateUrl: './dashboard.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export default class DashboardComponent {
-  date: Date | undefined;
+  formConfig = [
+    {
+      type: 'input',
+      label: '关键词',
+      placeholder: '请输入关键词',
+      prop: 'keyword',
+      formItem: ['', Validators.required],
+    },
+  ];
 }
