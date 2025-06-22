3deefcb63b079bbd7136670d132161a19737c66d
chenc <double_cl@163.com>
2025-06-22 20:52:03 +0800

    refactor(app): 优化代码格式和依赖版本
    - 简化 auth.interceptor.ts 中的代码结构
    - 更新 biome.json 中的格式设置
    - 在 package.json 中修正 @biomejs/biome 版本并移除 packageManager 配置

--- src/app/interceptors/auth.interceptor.ts
+++ src/app/interceptors/auth.interceptor.ts
@@ -5,9 +5,7 @@ import { environment } from '~/environments/environment';
 
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
 	const authToken = inject(AuthService).getAuthToken();
-	const headers = authToken
-		? req.headers.append('Authorization', `Bearer ${authToken}`)
-		: req.headers;
+	const headers = authToken ? req.headers.append('Authorization', `Bearer ${authToken}`) : req.headers;
 	const newReq = req.clone({
 		url: `${environment.baseUrl}${req.url}`,
 		headers: headers,
